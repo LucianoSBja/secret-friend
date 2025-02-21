dd75e2e630fc40ace52d5f91633fe211135d3033
Luciano Soto <lucianosotobonja95@gmail.com>
2025-02-21 13:54:23 -0300

    function to update friends

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let friends = [];
+
+function updateFriendList() {
+  const list = document.getElementById("listaAmigos");
+  list.innerHTML = ""; // Limpiar la lista antes de actualizar
+
+  for (let friend of friends) {
+    const listItem = document.createElement("li");
+    listItem.textContent = friend;
+    list.appendChild(listItem);
+  }
+}
